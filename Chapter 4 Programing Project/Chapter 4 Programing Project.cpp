3c9e13c38c8e4cc6ba570bf32f9ac57e871e5130
Lucian-77 <lucianshambhu@gmail.com>
2025-03-25 15:08:08 -0400

    Started

--- Chapter 4 Programing Project/Chapter 4 Programing Project.cpp
+++ Chapter 4 Programing Project/Chapter 4 Programing Project.cpp
@@ -6,65 +6,15 @@
 
 int main()
 {
-    std::cout << "Enter a month ";
-    float month;
-    std::cin >> month;
-    while(month - floor(month) > 0 || month < 1 || month > 12) {
-        std::cout << "Please enter an integer 1 - 12 ";
-        std::cin.clear();
-        std::cin.ignore(256, '\n');
-        std::cin >> month;
+    std::cout << "How much does the package weigh in Kilograms? ";
+    float weight;
+    std::cin >> weight;
+    while (weight <= 0) {
+        std::cout << "Please enter a weight between 1 and 20 kilos ";
     }
-    std::cout << "\nEnter a year ";
-    float year;
-    std::cin >> year;
-    while(year - floor(year ) > 0) {
-        std::cout << "Please enter an integer ";
-        std::cin.clear();
-        std::cin.ignore(256, '\n');
-        std::cin >> year;
-    }
-    int intMonth = month;
-    int intYear = year;
-    switch (intMonth) {
-    case 1: std::cout << "\n31 days\n";
-        break;
-    case 2: {
-        if ((intYear % 400) == 0) {
-            std::cout << "\n29 days\n";
-        }
-        else {
-            if ((intYear % 4) == 0) {
-                std::cout << "\n29 days\n";
-            }
-            else {
-                std::cout << "\n28 days\n";
-            }
-        }
-    }
-        break;
-    case 3: std::cout << "\n31 days\n";
-        break;
-    case 4: std::cout << "\n30 days\n";
-        break;
-    case 5: std::cout << "\n31 days\n";
-        break;
-    case 6: std::cout << "\n30 days\n";
-        break;
-    case 7: std::cout << "\n31 days\n";
-        break;
-    case 8: std::cout << "\n31 days\n";
-        break;
-    case 9: std::cout << "\n30 days\n";
-        break;
-    case 10: std::cout << "\n31 days\n";
-        break;
-    case 11: std::cout << "\n30 days\n";
-        break;
-    case 12: std::cout << "\n31 days\n";
-        break;
-    default: std::cout << "\nError\n";
-    }
+    std::cout << "How far do you wish to ship the package in miles? ";
+    float distance;
+    std::cin >> distance;
 
     return 0;
 }
