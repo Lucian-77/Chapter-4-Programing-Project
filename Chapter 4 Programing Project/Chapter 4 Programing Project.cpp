7ac864dcae7d61142405fb721e9175e561132c6f
Lucian-77 <lucianshambhu@gmail.com>
2025-03-25 18:10:53 -0400

    Shortened switch

--- Chapter 4 Programing Project/Chapter 4 Programing Project.cpp
+++ Chapter 4 Programing Project/Chapter 4 Programing Project.cpp
@@ -6,20 +6,20 @@
 
 int main()
 {
-    std::cout << "Enter a month ";
+    std::cout << "Enter a month: ";
     float month;
     std::cin >> month;
     while(month - floor(month) > 0 || month < 1 || month > 12) {
-        std::cout << "Please enter an integer 1 - 12 ";
+        std::cout << "Please enter an integer 1 - 12: ";
         std::cin.clear();
         std::cin.ignore(256, '\n');
         std::cin >> month;
     }
-    std::cout << "\nEnter a year ";
+    std::cout << "\nEnter a year: ";
     float year;
     std::cin >> year;
     while(year - floor(year ) > 0) {
-        std::cout << "Please enter an integer ";
+        std::cout << "Please enter an integer: ";
         std::cin.clear();
         std::cin.ignore(256, '\n');
         std::cin >> year;
@@ -27,41 +27,25 @@ int main()
     int intMonth = month;
     int intYear = year;
     switch (intMonth) {
-    case 1: std::cout << "\n31 days\n";
+    case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+        std::cout << "\nThis month has 31 days\n";
         break;
     case 2: {
         if ((intYear % 400) == 0) {
-            std::cout << "\n29 days\n";
+            std::cout << "\nThis month has 29 days\n";
         }
         else {
             if ((intYear % 4) == 0) {
-                std::cout << "\n29 days\n";
+                std::cout << "\nThis month has 29 days\n";
             }
             else {
-                std::cout << "\n28 days\n";
+                std::cout << "\nThis month has 28 days\n";
             }
         }
     }
         break;
-    case 3: std::cout << "\n31 days\n";
-        break;
-    case 4: std::cout << "\n30 days\n";
-        break;
-    case 5: std::cout << "\n31 days\n";
-        break;
-    case 6: std::cout << "\n30 days\n";
-        break;
-    case 7: std::cout << "\n31 days\n";
-        break;
-    case 8: std::cout << "\n31 days\n";
-        break;
-    case 9: std::cout << "\n30 days\n";
-        break;
-    case 10: std::cout << "\n31 days\n";
-        break;
-    case 11: std::cout << "\n30 days\n";
-        break;
-    case 12: std::cout << "\n31 days\n";
+    case 4: case 6: case 9: case 11:
+        std::cout << "\nThis month has 30 days\n";
         break;
     default: std::cout << "\nError\n";
     }
