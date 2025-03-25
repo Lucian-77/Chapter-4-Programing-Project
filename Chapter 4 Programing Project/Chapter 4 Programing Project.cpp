c0abda4378d258ba851483704d7c85c9b5e9226c
Lucian-77 <lucianshambhu@gmail.com>
2025-03-25 17:11:50 -0400

    started

--- Chapter 4 Programing Project/Chapter 4 Programing Project.cpp
+++ Chapter 4 Programing Project/Chapter 4 Programing Project.cpp
@@ -1,4 +1,4 @@
-// Chapter 4 Programing Project.cpp : This file contains the 'Shipping Charges' function. Program execution begins and ends there.
+// Chapter 4 Programing Project.cpp : This file contains the 'Area Calculator' function. Program execution begins and ends there.
 //
 
 #include <iostream>
@@ -7,57 +7,15 @@
 
 int main()
 {
-    std::cout << std::left << std::setw(68) << "How much does the package weigh in Kilograms? (20 kilo max)" << std::right << "Kg:    ";
-    float weight;
-    std::cin >> weight;
-    while (weight <= 0 || weight > 20) {
-        if (weight <= 0) {
-            std::cout << std::left << std::setw(68) << "Please enter a weight greater than 0 kilos " << std::right << "Kg:    ";
-            std::cin.clear();
-            std::cin.ignore(256, '\n');
-            std::cin >> weight;
-        }
-        if (weight > 20) {
-            std::cout << std::left << std::setw(68) << "Please enter a weight less than 20 kilos " << std::right << "Kg:    ";
-            std::cin.clear();
-            std::cin.ignore(256, '\n');
-            std::cin >> weight;
-        }
+    std::cout << "1. Calculate the Area of a Circle\n2. Calculate the Area of a Rectangle\n3. Calculate the Area of a Triangle\n4. Quit\nEnter your choice (1-4): ";
+    int choice;
+    std::cin >> choice;
+    while (choice < 1 || choice > 4) {
+        std::cout << "Please enter a selection 1 - 4 ";
+        std::cin.clear();
+        std::cin.ignore(256, \n);
+        std::cin >> choice;
     }
-    std::cout << std::left << std::setw(69) << "\nHow far do you wish to ship the package in miles? (3,000 mile max)" << std::right << "Miles: ";
-    float distance;
-    std::cin >> distance;
-    while (distance < 10 || distance > 3000) {
-        if (distance < 10) {
-            std::cout << std::left << std::setw(68) << "Please enter a distance greater than 10 miles " << std::right << "Miles: ";
-            std::cin.clear();
-            std::cin.ignore(256, '\n');
-            std::cin >> distance;
-        }
-        if (distance > 3000) {
-            std::cout << std::left << std::setw(68) << "Please enter a distance less than 3,000 miles " << std::right << "Miles: ";
-            std::cin.clear();
-            std::cin.ignore(256, '\n');
-            std::cin >> distance;
-        }
-    }
-    int weight2 = ceil(weight);
-    float cost = 1;
-    switch (weight2) {
-    case 1: case 2:
-        cost = (distance / 500) * 1.10;
-        break;
-    case 3: case 4: case 5: case 6:
-        cost = (distance / 500) * 2.20;
-        break;
-    case 7: case 8:case 9: case 10:
-        cost = (distance / 500) * 3.70;
-        break;
-    case 11: case 12: case 13: case 14: case 15: case 16: case 17: case 18: case 19: case 20:
-        cost = (distance / 500) * 4.80; 
-        break;
-    }
-    std::cout << std::fixed << std::setprecision(2) << "\nTo ship " << weight << "Kg " << distance << " miles, it will cost $" << cost << '\n';
 
     return 0;
 }
